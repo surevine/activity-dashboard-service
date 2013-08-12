c10ae985970f66c0fb22bda37916d87f75b2c950
Jonny Heavey <jonny.heavey@surevine.com>
2013-08-12 16:54:44 +0100

    Added license headers to #30

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+/**
+ * Copyright Surevine Ltd. 2013
+ *
+ * The code herein is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The code herein is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // Global Variables
 environment  = process.env.NODE_ENV || 'production';
 config = require('./config.'+environment+'.js'); 
