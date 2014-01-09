2609fe4e87ee4137a003a96587383cd3cd018423
Jonny Heavey <jonny.heavey@surevine.com>
2014-01-09 13:12:07 +0000

    fixes #57

--- modules/blog.js
+++ modules/blog.js
@@ -92,8 +92,7 @@ BlogMonitor.prototype.formatActivity = function(activity) {
 // Build HTML string for activity content
 BlogMonitor.prototype.formatActivityContentString = function(activity) {
   
-  var content = '<a href="'+activity['link']+'"><h2>'+activity['title']+'</h2></a>'+
-                '<a href="'+config.blog.author_url+activity.author+'">'+activity['author']+'</a>';
+  var content = '<a href="'+activity['link']+'"><h2>'+activity['title']+'</h2></a>';
                 if(activity['rss:description']['#'] != undefined) {
                   content += '<p>'+activity['rss:description']['#']+'</p>'+
                              '<a href="'+activity['link']+'">Read More</a>';
